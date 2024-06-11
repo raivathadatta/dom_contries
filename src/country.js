c7ad1388edf08b1e7c72dc2dde1aea412be49134
raivatha datta <raivatha.28.2@mountblue.tech>
2024-06-11 15:11:11 +0530

    contries

---
+++ src/country.js
@@ -0,0 +1,183 @@
+
+console.log(getQueryParams('country'))
+
+let countriesList = []
+let isDark = false
+
+let country_name = getQueryParams('country').toLowerCase()  
+
+let flag_image = document.getElementById("flag_image")
+let country_details = document.getElementById("country_details")
+let top_level_domine = document.getElementById("top_level_domine")
+let border_countries = document.getElementById("border_countries")
+let backButton = document.getElementById('backButton');
+let header = document.querySelector("header")
+let page_title = document.getElementById("pagetitle")
+let toggleButton = document.getElementById("toggl_background_button")
+const content = document.getElementById("screen");
+const loader = document.getElementById("loader");
+
+
+
+showLoader()
+loadCountryList()
+
+backButton.addEventListener('click', previousPage)
+toggleButton.addEventListener("click", changeBackgroundColor)
+
+
+page_title.innerText = "where in the world?"
+page_title.style = "font-weight:bold; font-size:30px"
+toggleButton.textContent = "Dark Mode"
+header.style.padding = "5%"
+
+
+
+
+function loadCountryList() {
+    fetch("https://restcountries.com/v3.1/all").then((responce) => {
+        console.log("fetched data ")
+
+        return responce.json()
+    }).then((countries) => {
+        console.log(countries.length)
+        console.log("joing data", countries.length)
+        countriesList = countries
+        countries.forEach((element) => {
+            // console.log(element['name']['common'].toLowerCase())
+            if (element['name']['common'].toLowerCase() == country_name) {
+                console.log(element['name']['common'].toLowerCase())
+
+                createCountryDetailCard(element)
+
+                showContent()
+            }
+        });
+
+    }).catch((error) => { console.log(error) })
+
+}
+
+
+function changeBackgroundColor() {
+    if (isDark) {
+        document.body.style = " color:black"
+        header.style.backgroundColor = "#FFFFFF"
+        screen_body.style.backgroundColor = "#FAFAFA"
+        toggleButton.style.backgroundColor = "#FAFAFA"
+        toggleButton.innerHTML = "Light Mode"
+
+
+        isDark = false
+    } else {
+        document.body.style = " color:white"
+        toggleButton.innerHTML = "Dark Mode"
+        toggleButton.style = "background-color: #444444;color: #ffffff;"
+        header.style = "background-color: #1f1f1f;border-color: #444444;"
+        toggleButton.style.backgroundColor = "#202D36"
+        screen_body.style.backgroundColor = "#202D36"
+
+        isDark = true
+
+
+    }
+}
+
+
+
+
+
+
+
+function previousPage() {
+
+    window.history.back();
+
+}
+
+function createCountryDetailCard(element) {
+    console.log(element['name']['common'].toLowerCase(), element['name'])
+
+    flag_image.src = element['flags']['png']
+    ////country_details div 
+    let countryName = document.createElement('h1')
+    let population = document.createElement('p')
+    let native_name = document.createElement('p')
+
+    let region = document.createElement('p')
+    let subregion = document.createElement('p')
+    let capital = document.createElement('p')
+
+    countryName.innerText = element['name']['common']
+    native_name.innerText = " Native Name :" + Object.values(element['name']['nativeName'])['official']///official
+    population.innerText = "Population :" + element['population']
+    region.innerText = "Region :" + element['region']
+    subregion.innerText = "Sub Region" + element['subregion']
+    capital.innerText = "Capital" + element['capital']
+    country_details.appendChild(countryName)
+    country_details.appendChild(native_name)
+    country_details.appendChild(population)
+    country_details.appendChild(region)
+    country_details.appendChild(subregion)
+    country_details.appendChild(capital)
+
+
+
+    let topLevelDomain = element['tld']
+    let currencies = Object.keys(element['currencies']).toString()
+    let languages = Object.values(element['languages']).toString()
+    console.log(languages)
+
+    let topleveldomain = document.createElement('p')
+    let currencies_list = document.createElement('p')
+    let languages_list = document.createElement('p')
+
+    topleveldomain.innerHTML = `<span class="font-medium">Top Level Domain</span> :${topLevelDomain}`
+    currencies_list.innerHTML = `<span class="font-medium">Currencies</span> :${currencies}`
+    languages_list.innerHTML = `<span class="font-medium">Languages</span> :${languages}`
+
+    top_level_domine.appendChild(topleveldomain)
+    top_level_domine.appendChild(currencies_list)
+    top_level_domine.appendChild(languages_list)
+
+    let borders = element['borders']
+
+
+    borders.forEach((border) => {
+        let border_name = document.createElement('button')
+        border_name.addEventListener('click', navigateToCountry)
+        border_name.className = "border_button"
+        border_name.style = "font-weight : 300;     border: 1px solid; padding:2%"
+        border_name.style.padding = " 1px 6px 1px 6px;"
+
+        border_name.innerText = border
+        border_countries.appendChild(border_name)
+    })
+}
+
+function getQueryParams(param) {
+    let queryString = window.location.search
+    let urlParams = new URLSearchParams(queryString)
+    return urlParams.get(param)
+}
+function navigateToCountry(event) {
+    let selected_country = event.target.textContent.toLowerCase()
+    countriesList.forEach((country) => {
+        console.log(country['cca3'].toLowerCase(), "hhhhhhhhhhh")
+        if (country['cca3'].toLowerCase() == selected_country) {
+            console.log(country['name']['common'])
+            window.location.href = "country.html?country=" + country['name']['common']
+        }
+    })
+}
+
+function showLoader() {
+    loader.style.display = "block";
+    content.style.display = "none";
+}
+
+function showContent() {
+    console.log("show content");
+    loader.style.display = "none";
+    content.style.display = "block";
+}
---
+++ src/index.js
@@ -0,0 +1,248 @@
+// this function calls when all the html is loadedv
+
+showLoader()
+fetchCountryList()
+console.log("all html is loaded")
+
+
+//lists and varibles 
+let isDark = false
+let region_list = ["Filter by Region"]
+let counter = 0
+let totalCountries = 0
+let countriesList = []
+
+
+
+/// elements
+let countries_grid = document.getElementById("counties_grid")
+let page_title = document.getElementById("pagetitle")
+let toggleButton = document.getElementById("toggl_background_button")
+let searchInput = document.getElementById("search")
+let filter_button = document.getElementById("filter_button")
+let header = document.querySelector("header")
+let screen_body = document.getElementById("screen_body")
+let filter_main = document.getElementById("filter_main")
+
+/// html modifications
+page_title.innerHTML = "where in the world?"
+page_title.style = "font-weight:bold; font-size:30px"
+toggleButton.textContent = "Dark Mode"
+
+
+
+///event listners 
+searchInput.addEventListener("keyup", searchCountry)
+filter_button.addEventListener("click", filterCountries)
+
+
+
+
+toggleButton.addEventListener("click", changeBackgroundColor)
+
+
+function changeBackgroundColor() {
+    if (isDark) {
+        document.body.style = " color:black"
+        header.style.backgroundColor = "#FFFFFF"
+        screen_body.style.backgroundColor = "#FAFAFA"
+        toggleButton.style.backgroundColor = "#FAFAFA"
+        searchInput.style.backgroundColor = "#FAFAFA"
+        filter_button.style.backgroundColor = "#FAFAFA"
+        toggleButton.innerHTML = "Light Mode"
+        toggleButton.style = "color:black;"
+
+        Array.from(countries_grid.children).forEach((country_card) => {
+            console.log(country_card)
+            country_card.style.backgroundColor = "#ffffff"
+        })
+        isDark = false
+    } else {
+        document.body.style = " color:white"
+        toggleButton.innerHTML = "Dark Mode"
+        toggleButton.style = "background-color: #444444;color: #ffffff;"
+        header.style = "background-color: #1f1f1f;border-color: #444444;"
+        toggleButton.style.backgroundColor = "#202D36"
+        screen_body.style.backgroundColor = "#202D36"
+        searchInput.style = " background-color: #444444; border-none border-width:0px"
+        filter_button.style = "background-color: #444444;"
+        filter_main.style.backgroundColor = "background-color: #1f1f1f;"
+
+        Array.from(countries_grid.children).forEach((country_card) => {
+            console.log(country_card)
+            country_card.style.backgroundColor = "#2B3743"
+        })
+        isDark = true
+
+
+    }
+}
+
+
+/////////////////////
+//functions
+function fetchCountryList() {
+    fetch("https://restcountries.com/v3.1/all").then((responce) => {
+        console.log("fetched data ")
+
+        return responce.json()
+    }).then((countries) => {
+        console.log(countries.length)
+        // console.log("joing data", countries.length)
+        totalCountries = countries.length
+        countriesList = countries
+        countries.forEach((element) => {
+
+            createCountryCard(element)
+        });
+        showContent()
+
+    }).catch((error) => { console.log(error) })
+
+}
+
+function createCountryCard(element) {
+    // console.log("element: " + element)
+    let countryCard = document.createElement("div")
+    let flag = document.createElement("img")
+    let countryName = document.createElement("h2")
+    let capital = document.createElement("p")
+    let population = document.createElement("p")
+    let region = document.createElement("p")
+    region.id = "country_region"
+    countryName.style = "font-weight : bold;"
+    countryName.className = "mb-2 mt-2"
+    population.style = "font-weight : 300;"
+    region.style = "font-weight :300;"
+    capital.style = "font-weight :300;"
+
+    flag.className = "w-[100%]  block  top-left h-[50%] mb-2";
+    flag.src = element['flags']['png']
+    countryName.innerText = element['name']['common']
+    capital.innerText = "Capital: " + element['capital']
+    population.innerText = "Population: " + element['population']
+    region.innerText = "Region: " + element['region']
+
+    countryCard.className = "w-[23%]  p-[3%]  m-2 w-[90%]"
+
+    countryCard.appendChild(flag)
+    countryCard.appendChild(countryName)
+    countryCard.appendChild(capital)
+    countryCard.appendChild(population)
+    countryCard.appendChild(region)
+    countries_grid.appendChild(countryCard)
+    countryCard.addEventListener('click', navigateToCountry)
+
+
+    if (!region_list.includes(element['region'])) {
+        region_list.push(element['region'])
+    }
+    counter++
+    if (counter == totalCountries) {
+        createSelectCountry()
+    }
+
+    console.log(region_list)
+
+}
+
+
+
+function searchCountry() {
+    let search_country = searchInput.value.toLowerCase()
+    let filter_button_value = filter_button.value.toLowerCase()
+
+
+    if (filter_button_value == region_list[0].toLocaleLowerCase()) {
+
+        Array.from(countries_grid.children).forEach((countryCard) => {
+            let country = countryCard.querySelector('h2').textContent.toLowerCase()
+              if (country.indexOf(search_country) == -1) {
+                countryCard.classList.add('hidden')
+            } else {
+                countryCard.classList.remove('hidden')
+            }
+        })
+    } else {
+        console.log(filter_button_value, region_list[0].toLocaleLowerCase())
+        Array.from(countries_grid.children).forEach((countryCard) => {
+            let country = countryCard.querySelector('h2').textContent.toLowerCase()
+            let country_region = countryCard.querySelector('#country_region')
+            if (country.indexOf(search_country) != -1 && country_region.classList.contains('hidden')) {
+
+                countryCard.classList.remove('hidden')
+            } else {
+                countryCard.classList.add('hidden')
+            }
+        })
+    }
+
+
+}
+
+
+function showAllCountries() {
+    Array.from(countries_grid.children).forEach((countryCard) => {
+        countryCard.classList.remove('hidden')
+    })
+}
+
+function createSelectCountry() {
+    region_list.forEach((region) => {
+        console.log(region)
+        let option = document.createElement("option")
+        option.value = region
+        option.innerText = region
+        filter_button.appendChild(option)
+        console.log(region)
+    })
+}
+
+function filterCountries() {
+    let selectedItem = filter_button.value.toLowerCase()//country_region
+    if (selectedItem == region_list[0].toLowerCase()) {
+
+        showAllCountries()
+    } else {
+        Array.from(countries_grid.children).forEach((countryCard) => {
+            let country_region = countryCard.querySelector('#country_region').textContent.toLowerCase()
+            console.log(country_region)
+            if (country_region.indexOf(selectedItem) == -1) {
+                countryCard.classList.add('hidden')
+            } else {
+                countryCard.classList.remove('hidden')
+            }
+        })
+    }
+
+}
+
+
+function navigateToCountry(event) {
+
+    let selected_country = event.target.parentElement.querySelector('h2').textContent.toLowerCase()
+    console.log(event.target.parentElement.querySelector('h2').textContent.toLowerCase() + ' selected')
+    countriesList.forEach((country) => {
+        if (country['name']['common'].toLowerCase() == selected_country) {
+            console.log(country['name']['common'])
+            window.location.href = "country.html?country=" + country['name']['common']
+        }
+    })
+}
+
+
+function showLoader() {
+    const loader = document.getElementById("loader");
+    loader.style.display = "block";
+    const content = document.getElementById("screen");
+    content.style.display = "none";
+}
+
+function showContent() {
+    console.log()
+    const loader = document.getElementById("loader");
+    loader.style.display = "none";
+    loader.classList.add("hidden")
+    const content = document.getElementById("screen");
+    content.style.display = "block";
+}
